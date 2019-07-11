7bef8adda8111120ee3f4d7858067dc9d1758037
Jeff Prehm <japrehm@gmail.com>
2019-07-11 15:54:47 -0500

    laravel 5.8 vue-material-kit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,7 @@
 
         <!-- Styles -->
         @yield('style')
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-        <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
-        <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <!-- Head -->
         @yield('head')
